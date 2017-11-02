4214269f7360aaa8dbf081b5ec9f767eaf40da3c
DIllonhill123 <dillonhill9@gmail.com>
2017-11-02 14:17:04 -0400

    TicTacToe

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,38 +10,52 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            String[,] board = new string[8, 8];
+            String[,] board = new String[4, 4];
             string input;
+            int row;
+            int col;
 
-            board[0, 3] = " A ";
-            board[0, 5] = " B ";
-            board[0, 7] = " C ";
-            board[3, 0] = "1";
-            board[5, 0] = "2";
-            board[7, 0] = "3";
-            board[1, 3] = "-";
-            board[1, 5] = "-";
-            board[1, 7] = "-";
-            board[1, 2] = "+-";
-            board[1, 4] = "-+-";
-            board[1, 6] = "-+";
-            board[2, 2] = "+-";
-            board[2, 4] = "-+-";
-            board[2, 6] = "-+";
+            board[0, 1] = "  A ";
+            board[0, 2] = " B ";
+            board[0, 3] = " C ";
+            board[1, 0] = "1";
+            board[2, 0] = "2";
+            board[3, 0] = "3";
 
+            board[1, 1] = " _ ";
+            board[1, 2] = " _ ";
+            board[1, 3] = " _ ";
 
+            board[2, 1] = " _ ";
+            board[2, 2] = " _ ";
+            board[2, 3] = " _ ";
+
+            board[3, 1] = " _ ";
+            board[3, 2] = " _ ";
+            board[3, 3] = " _ ";
+
+            while (true)
+            {
+                printboard(board);
+
+                Console.WriteLine("Player X where would you like to move: ");
+                input = Console.ReadLine();
+                row = Convert.ToInt32(input[0]);
+
+                if(input)
+            }
 
 
         }
-        public void printboard(String[,] board)
+        public static void printboard(String[,] board)
         {
-            for(int i = 0; i < board.Length; i++)
+            for(int i = 0; i < board.GetLength(0); i++)
             {
-                for(int j = 0; j < board.Length; j++)
+                for(int j = 0; j < board.GetLength(1); j++)
                 {
                     Console.Write(board[i, j]);
-                    Console.WriteLine();
                 }
+                Console.WriteLine();
             }
         }
         public void update(String[,] board, int row, int col, string player)
@@ -88,23 +102,23 @@ namespace TicTacToe
         }
         public bool checkRow(String[,] board, int row, char player) 
         {
-            if (player == board[row, 3][0] && board[row, 3] == board[row, 5] && board[row, 5] == board[row, 7])
+            if (player == board[row, 1][0] && board[row, 1] == board[row, 2] && board[row, 2] == board[row, 3])
                 return true;
 
             return false;
         }
         public bool checkcol(String[,] board, int col, char player)
         {
-            if (player == board[col, 3][0] && board[col, 3] == board[col, 5] && board[col, 5] == board[col, 7])
+            if (player == board[col, 1][0] && board[col, 1] == board[col, 2] && board[col, 2] == board[col, 3])
                 return true;
 
             return false;
         }
         public bool checkdiag(String[,] board, char player)
         {
-            if (player == board[3, 3][0] && board[3, 3] == board[5, 5] && board[5, 5] == board[7, 7])
+            if (player == board[1, 1][0] && board[1, 1] == board[2, 2] && board[2, 2] == board[3, 3])
                 return true;
-            if (player == board[3, 7][0] && board[3, 7] == board[5, 5] && board[5, 5] == board[7, 3])
+            if (player == board[1, 3][0] && board[1, 3] == board[2, 2] && board[2, 2] == board[3, 1])
                 return true;
 
             return false;
